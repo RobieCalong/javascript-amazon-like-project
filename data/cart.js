224036b351102a43a3e966b59f098c6208313afe
robC_vW <robiecalong2@gmail.com>
2025-05-26 15:11:28 -0500

    Move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,20 @@
 export const cart = [];
+
+export function addToCart(productId) {
+  let matachingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matachingItem = cartItem;
+    }
+  });
+
+  if (matachingItem) {
+    matachingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+    });
+  }
+}
